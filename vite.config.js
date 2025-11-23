7dfbea2091eb7fa995c92a330f08c3437662832f
Bruno <12522178550@ulife.com.br>
2025-11-23 14:37:14 -0300

    fix: replace a archive

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 // We use a custom project root inside /src to match the requested structure
 export default defineConfig({
+  base: "./",
   root: "src",
   publicDir: "../public",
   build: {
